df1857f6feb0235ab29cb3651105cd3124da1ac7
dvsrshohan <shohan.ridigames@gmail.com>
2022-11-11 23:05:33 +0600

    Update Home pgae Slider Baner

--- src/components/Home/Slider.js
+++ src/components/Home/Slider.js
@@ -16,6 +16,7 @@ const Slider = () => {
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
+
             <div id="slide2" className="carousel-item relative w-full">
                 <PhotoProvider>
                     <PhotoView src="https://www.abusufiannilove.com/wp-content/uploads/2018/05/NIL08317-1024x522.jpg" >
@@ -28,6 +29,7 @@ const Slider = () => {
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
+
             <div id="slide3" className="carousel-item relative w-full">
                 <PhotoProvider>
                     <PhotoView src="https://www.abusufiannilove.com/wp-content/uploads/2018/05/NIL_0641-1024x522.jpg" >
@@ -40,6 +42,7 @@ const Slider = () => {
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
+            
             <div id="slide4" className="carousel-item relative w-full">
                 <PhotoProvider>
                     <PhotoView src="https://www.abusufiannilove.com/wp-content/uploads/2018/05/NIL05259-1024x522.jpg" >
